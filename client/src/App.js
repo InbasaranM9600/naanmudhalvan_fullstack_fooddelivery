5e14b42024850987744a158e669894b1ddec8443
InbasaranM <sarkarsaran9600@gmail.com>
2023-11-06 23:26:56 +0530

    app

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  return <div className="text-blue-500 font-semibold">App</div>;
+};
 
-export default App
+export default App;
