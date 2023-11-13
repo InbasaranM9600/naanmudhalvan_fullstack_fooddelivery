72ea69ff6f9409b45cff4e4ac72be46075980738
InbasaranM <sarkarsaran9600@gmail.com>
2023-11-13 23:46:05 +0530

    todays last

--- client/src/context/reducers/index.js
+++ client/src/context/reducers/index.js
@@ -1,20 +1,9 @@
 import { combineReducers } from "redux";
-import alertReducer from "./alertReducer";
 import userReducer from "./userReducer";
-import productReducer from "./productReducer";
-import allUserReducer from "./allUserReducer";
-import cartReducer from "./cartReducer";
-import displayCartReducer from "./displayCartReducer";
-import ordersReducer from "./ordersReducer";
+
 
 const myReducers = combineReducers({
-  user: userReducer,
-  alert: alertReducer,
-  products: productReducer,
-  allUsers: allUserReducer,
-  cart: cartReducer,
-  isCart: displayCartReducer,
-  orders: ordersReducer,
+  user: userReducer
 });
 
 export default myReducers;
