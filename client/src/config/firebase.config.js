bb1bab6b3574139258f862ce43e0187788f327c1
InbasaranM <sarkarsaran9600@gmail.com>
2023-11-13 14:03:56 +0530

    config

--- client/src/config/firebase.config.js
+++ client/src/config/firebase.config.js
@@ -6,12 +6,12 @@ import {getStorage} from "firebase/storage";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCUzW28cr1QOpItj12SA_hBKVn1d2jTW60",
-    authDomain: "fullstack-food-app-dd8f7.firebaseapp.com",
-    projectId: "fullstack-food-app-dd8f7",
-    storageBucket: "fullstack-food-app-dd8f7.appspot.com",
-    messagingSenderId: "391499505910",
-    appId: "1:391499505910:web:4adb9de9d9b3d4c2660cd7"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSEAGING_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
