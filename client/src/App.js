8a5d527071380ac18ee916bfe07b9dd91d640a9a
InbasaranM <sarkarsaran9600@gmail.com>
2023-11-17 11:01:05 +0530

    today

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import {Route, Routes} from "react-router-dom";
 import { Login, Main } from './containers';
-
+import { getAuth } from 'firebase/auth';
+import { app } from './config/firebase.config';
+import { validateUserJWTToken } from "./api";
+import { useDispatch } from 'react-redux';
+import { setUserDetails } from "./context/actions/userActions";
+import { motion } from "framer-motion";
+import { fadeInOut } from "./animations";
 
 const App = () => {
+
+  const firebaseAuth = getAuth(app);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const dispatch = useDispatch();
+  
+  useEffect(() => {
+    setIsLoading(true);
+    firebaseAuth.onAuthStateChanged(cred => {
+      if(cred){
+        cred.getIdToken().then(token => {
+          validateUserJWTToken(token).then(data => {
+            //console.log(data);
+            dispatch(setUserDetails(data));
+          });          
+        });
+      }
+      setInterval(() => {
+        setIsLoading(false);
+      }, 3000);
+    });
+  }, []);
+
+  
+  
+  
+
+  
   return (
     <div className="w-screen min-h-screen h-auto flex flex-col items-center justify-center">
+      {isLoading && (
+        <motion.div {...fadeInOut} className="fixed z-50 inset-0 bg-lightOverlay backdrop-blur-md flex items-center justify-center w-full">
+          loading...
+        </motion.div>
+      )}
       <Routes>
         <Route path="/*" element={<Main />} />
         <Route path="/login" element={<Login />} />
